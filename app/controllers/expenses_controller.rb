d6caea8390060a3c6d03d9eb97d6e5b4a6a51b5a
saxgutierrez <diegofdog@gmail.com>
2018-02-20 11:49:51 -0500

    haciendo filtro por palabras y por id

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,13 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+     @expenses = Expense.order("date DESC") # ordenar por fecha descendente en la tabla de la base de datos
+     if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%") # el where busca es igual que el find el ILIKE para ignorar mayúsculas y minúsculas, busca texto
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id]) #busca el id
+    end
   end
-end
+  end
+
+# el = y el ILIKE son operadores
