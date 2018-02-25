6193cd3ff23003164e8a4a40e31618e5111e1286
saxgutierrez <diegofdog@gmail.com>
2018-02-24 21:36:14 -0500

    agregando asociación entre user y expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Expense < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
